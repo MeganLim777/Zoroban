c48420a66eb4b9ff853dfa9b460430765d4cc0d4
MeganLim <meganlimlm@gmail.com>
2021-02-10 20:55:55 +1300

    Sorted out the main configurations for the Hooray page.
    . When the game is NOT over, the Game page is displayed.
    . When the game IS over, the Hooray page is displayed.
    . Determining whether the game is over or not is by the state of the 
    'gameOver' state variable of this GameScreen class component.
    
    . When the Game page is displayed, the HeaderBackButton and Android's 
    hardware Back button will BOTH display the alert asking for forfeit 
    confirmation.
    
    . When the Hooray page is displayed, ONLY the HeaderBackButton will 
    display the alert. Pressing the Android hardware Back button will NOT 
    display the alert.
         . This should be fine for now as we might take away the 
    GameScreen's navigation header thingy - so the HeaderBackButton may be 
    gone in the future.

--- abacu/screens/GameScreen/GameScreen.js
+++ abacu/screens/GameScreen/GameScreen.js
@@ -3,31 +3,48 @@ import {View, Text, SafeAreaView, Button, Alert, BackHandler} from 'react-native
 import {HeaderBackButton} from 'react-navigation-stack';
 import { withNavigation } from 'react-navigation';
 
-// //Function that will display an Alert when the user presses the
-// //top-left Back button.
-// //The Alert will ask whether the user wants to forfeit the game or not.
+//Function that IF the game is NOT over, then it will display
+//an Alert when the user presses the top-left Back button or display the
+//same Alert when Android hardware Back button is pressed; AND this function
+//also returns true.
+//The Alert will ask whether the user wants to forfeit the game or not.
+//If the game IS over, then there's no Alert and false is returned.
 const forfeitConfirmation = (navigation, gameOver) => {
 
-  Alert.alert(
-    'Are you sure you want to go back?',
-    'You will forfeit the game.',
-    [
-      {
-        text: 'No',
-        onPress: () => {},
-        style: 'cancel'
-      },
-      {
-        text: 'Yes',
-        style: 'destructive',
-        onPress: () => {
-          navigation.goBack();
-        }
-      }
-    ]
-  );
+  //If the game is NOT over, then:
+  // . Display an alert
+  // . Return true for the goBack function to return true for
+  //   adding the event listener to BackHandler.
+  if (!gameOver) {
 
-  return;
+    Alert.alert(
+      'Are you sure you want to go back?',
+      'You will forfeit the game.',
+      [
+        {
+          text: 'No',
+          onPress: () => {},
+          style: 'cancel'
+        },
+        {
+          text: 'Yes',
+          style: 'destructive',
+          onPress: () => {
+            navigation.goBack();
+          }
+        }
+      ]
+    );
+
+    return true;
+
+  } else {
+    //If the game IS over, then:
+    // . Return false for the goBack function to return false for
+    //   removing the event listener from BackHandler.
+    return false;
+  }
+
 }
 
 
@@ -35,6 +52,10 @@ class GameScreen extends Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      gameOver: false
+    }
   }
 
   //Call the forfeitConfirmation function when the
@@ -54,8 +75,7 @@ class GameScreen extends Component {
 
   //Function to call the forfeitConfirmation function.
   goBack = () => {
-    forfeitConfirmation(this.props.navigation);
-    return true;
+    return forfeitConfirmation(this.props.navigation, this.state.gameOver);
   }
 
   //Adding the event listener for Android's hardware Back button.
@@ -69,12 +89,36 @@ class GameScreen extends Component {
   }
 
   render() {
+
+    //By default let the Game page be displayed
+    let pageToDisplay = (
+      <View>
+        <Text>This is the Game screen</Text>
+      </View>
+
+    );
+
+    //If the game IS over
+    if (this.state.gameOver) {
+
+      //Remove the event listener of the Android's hardware Back button.
+      BackHandler.removeEventListener('hardwareBackPress', this.goBack);
+
+      //Change the pageToDisplay variable so that it displays the Hooray page
+      //instead.
+      pageToDisplay = (
+        <View>
+          <Text>Hooray!!!</Text>
+          <Button
+            title="Go to Main screen"
+            onPress={() => this.props.navigation.navigate('Main')} />
+        </View>
+      );
+    }
+
     return(
       <SafeAreaView>
-        <Text>This is the Game screen</Text>
-        <Button
-          title="Go to Main screen"
-          onPress={() => this.props.navigation.navigate('Main')}/>
+        {pageToDisplay}
       </SafeAreaView>
     );
   }
